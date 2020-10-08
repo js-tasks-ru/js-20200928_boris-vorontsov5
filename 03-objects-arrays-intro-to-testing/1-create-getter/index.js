9bf1f9f32c4ef0f7e2170e6c89e5d2582f193246
Boris Vorontsov <b.vorontsov@netris.ru>
2020-10-08 08:01:07 +0300

    implemented function createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,26 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(path) {
+  return (object) => {
+    if (!object) {
+      return object;
+    }
+
+    const parsedPath = path.split('.');
+
+    switch (parsedPath.length) {
+
+    case 0:
+      return object;
+
+    case 1:
+      return object[parsedPath[0]];
+
+    default:
+      return createGetter(parsedPath.slice(1).join('.'))(object[parsedPath[0]]);
+    }
+  };
 }
+
